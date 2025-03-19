d1ab332427cdf5e58c27aad9e7be3e61e837ba43
Sergey Borysenko <s.borysenko@rickerlyman.com>
2025-03-19 15:24:21 +0200

    Move REST API to using H2 database

--- backend/src/main/java/com/itera/cunsultantscheduler/repository/AssignmentRepository.java
+++ backend/src/main/java/com/itera/cunsultantscheduler/repository/AssignmentRepository.java
@@ -4,4 +4,5 @@ import com.itera.cunsultantscheduler.model.Assignment;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AssignmentRepository extends CrudRepository<Assignment, Long> {
+    Iterable<Assignment> getAllByConsultantId(Long id);
 }
--- backend/src/main/java/com/itera/cunsultantscheduler/service/BookingService.java
+++ backend/src/main/java/com/itera/cunsultantscheduler/service/BookingService.java
@@ -1,20 +1,21 @@
 package com.itera.cunsultantscheduler.service;
 
-import com.itera.cunsultantscheduler.dto.ConsultantInput;
-import com.itera.cunsultantscheduler.dto.CustomerInput;
+import com.itera.cunsultantscheduler.dto.*;
 import com.itera.cunsultantscheduler.model.Assignment;
 import com.itera.cunsultantscheduler.model.Consultant;
 import com.itera.cunsultantscheduler.model.Customer;
 import com.itera.cunsultantscheduler.model.ForecastDay;
 import com.itera.cunsultantscheduler.repository.*;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Set;
+import java.time.temporal.WeekFields;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -38,7 +39,7 @@ public class BookingService {
         this.forecastDayRepository = forecastDayRepository;
     }
 
-    public Iterable<Consultant> getAllConsultants() {
+    public List<Consultant> getAllConsultants() {
         log.info("Get all consultants");
         return StreamSupport
                 .stream(consultantRepository.findAll().spliterator(), false)
@@ -106,4 +107,104 @@ public class BookingService {
                 .hoursWorked(hoursWorked)
                 .build());
     }
+
+    public List<Booking> getAllBookings() {
+        List<Booking> bookings = new ArrayList<>();
+        Iterable<Consultant> consultants = consultantRepository.findAll();
+        consultants.forEach(consultant -> bookings.add(buildConsultantBooking(consultant)));
+        return bookings;
+    }
+
+    private Booking buildConsultantBooking(Consultant consultant) {
+        return Booking.builder()
+                .consultant(ConsultantDtd.builder()
+                        .id(consultant.getId())
+                        .name(consultant.getName())
+                        .hoursPerDay(consultant.getHoursPerDay())
+                        .ratePerHour(consultant.getRatePerHour())
+                        .build())
+                .assignments(buildConsultantAssignments(consultant))
+                .build();
+    }
+
+    private List<AssignmentDto> buildConsultantAssignments(Consultant consultant) {
+        Iterable<Assignment> assignments = assignmentRepository.getAllByConsultantId(consultant.getId());
+
+        List<AssignmentDto> dtos = new ArrayList<>();
+        assignments.forEach(a -> dtos.add(buildConsultantAssignment(a, consultant)));
+        return dtos;
+    }
+
+    private AssignmentDto buildConsultantAssignment(Assignment assignment, Consultant consultant) {
+        Customer customer = customerRepository.findById(assignment.getCustomer().getId()).orElseThrow();
+        RevenueHolder revenue = collectRevenue(assignment, consultant);
+
+        return AssignmentDto.builder()
+                .id(assignment.getId())
+                .name(assignment.getName())
+                .revenue(revenue.getRevenue())
+                .hoursWorked(revenue.getHours())
+                .customer(CustomerDto.builder().id(customer.getId()).name(customer.getName()).build())
+                .revenueMonth(revenue.getMonths())
+                .build();
+    }
+
+    private RevenueHolder collectRevenue(Assignment assignment, Consultant consultant) {
+        Map<Month, Map<Integer, List<ForecastDay>>> groupedData = assignment.getForecastDays().stream()
+                .collect(Collectors.groupingBy(
+                        record -> record.getDate().getMonth(), // Group by month
+                        Collectors.groupingBy(
+                                record -> record.getDate().get(WeekFields.of(Locale.getDefault()).weekOfMonth()), // Group by week
+                                Collectors.toList() // Group by day within week
+                        )
+                ));
+
+        RevenueHolder holder = new RevenueHolder();
+
+        // Print the grouped structure
+        groupedData.forEach((month, weeks) -> {
+            RevenueMonth rm = new RevenueMonth();
+            rm.setName(month.name());
+
+            weeks.forEach((week, days) -> {
+                RevenueWeek rw = new RevenueWeek();
+                rw.setWeekNumber(week);
+
+                days.forEach(day -> {
+                    RevenueDay rd = new RevenueDay();
+                    rd.setRevenue(consultant.getRatePerHour() * day.getHoursWorked());
+                    rd.setHours(day.getHoursWorked());
+                    rd.setDay(day.getDate().format(DateTimeFormatter.ISO_LOCAL_DATE));
+
+                    holder.addHours(day.getHoursWorked());
+                    rw.setRevenue(rw.getRevenue() + rd.getRevenue());
+                    rw.getDays().add(rd);
+                });
+
+                rm.setRevenue(rm.getRevenue() + rw.getRevenue());
+                rm.getWeeks().add(rw);
+            });
+
+            holder.addRevenue(rm.getRevenue());
+            holder.getMonths().add(rm);
+        });
+
+        return holder;
+    }
+
+    @Data
+    private static class RevenueHolder {
+        private Double revenue = 0.0;
+        private Integer hours = 0;
+        private List<RevenueMonth> months = new ArrayList<>();
+
+        private void addRevenue(Double revenue) {
+            this.revenue = this.revenue + revenue;
+        }
+
+        private void addHours(Integer hours) {
+            this.hours = this.hours + hours;
+        }
+    }
+
 }
